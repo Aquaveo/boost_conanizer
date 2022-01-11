07a2afe03bbe66e6145dbc2d392feda920c5fa6f
Thomas Christiansen <tchristiansen@aquaveo.local>
2022-01-11 13:58:56 -0700

    Add building for Windows and Mac.

--- build.py
+++ build.py
@@ -2,13 +2,7 @@ import os
 
 from cpt.packager import ConanMultiPackager
 
-
-if __name__ == "__main__":
-    # ConanPackageTools
-    # See: https://github.com/conan-io/conan-package-tools/blob/develop/README.md
-    builder = ConanMultiPackager()
-    builder.add_common_builds()
-
+def update_items_linux(builder):
     for settings, options, env_vars, build_requires, reference in builder.items:
         # Require c++11 compatibility
         if settings['compiler'] == 'gcc':
@@ -24,5 +18,29 @@ if __name__ == "__main__":
                 raise RuntimeError('Compiler must be GCC 6, 7, or 8')
         else:
             raise RuntimeError('Compiler must be GCC 6, 7, or 8')
+
+
+def update_items_mac(builder):
+   pass
+
+
+def update_items_windows(builder):
+    pass
+
+
+if __name__ == "__main__":
+    # ConanPackageTools
+    # See: https://github.com/conan-io/conan-package-tools/blob/develop/README.md
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
+
+    if os.environ['AQUA_OS'] == 'linux':
+        update_items_linux(builder)
+    elif os.environ['AQUA_OS'] == 'mac':
+        update_items_mac(builder)
+    elif os.environ['AQUA_OS'] == 'windows':
+        update_items_windows(builder)
+    else:
+        raise RuntimeError("Unknown operating system: " + str(os.environ['AQUA_OS']))
 
     builder.run()
